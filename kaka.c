3cdcefc00ea5facb734afc93f469205e3e651a8c
Kechida98 <kechida.ok@gmail.com>
2024-02-18 13:48:40 +0100

    Done

--- kaka.c
+++ kaka.c
@@ -1,1 +1,14 @@
-//hej hej 
+
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a =55;
+
+    int *ptr1=&a;
+
+
+printf("The value a is %d\n", a);
+printf("The value of a is also %d\n", *ptr1);
+}
